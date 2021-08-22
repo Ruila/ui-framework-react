c340de944980fe9200c0c76c0ce65b82c597b628
mengru <william.lin@nextdrive.io>
2021-08-22 21:33:14 +0800

    change color to test

--- wrap-materials/src/lib/MoButton.js
+++ wrap-materials/src/lib/MoButton.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 
 const styles = {
     root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        background: '#30b3ff',
         border: 0,
         borderRadius: 3,
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
